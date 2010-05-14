340c4914b01f501e2fc26a101d5b402a50c5fa9e
Keith Erskine <keith@macatron.local>
2010-05-14 14:22:23 -0400

    Added support for sending from a form

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,4 +1,14 @@
 class MessagesController < ApplicationController
+
+require 'twiliolib'
+
+    # Twilio REST API version
+    API_VERSION = '2008-08-01'
+
+    # Twilio AccountSid and AuthToken
+    ACCOUNT_SID = 'AC8eddd770e69ad61de3fef0b4671bd62d'
+    ACCOUNT_TOKEN = 'f1b8d4fa45a5b275494be3b1abf14f2b'
+
   # GET /messages
   # GET /messages.xml
   def index
@@ -24,7 +34,7 @@ class MessagesController < ApplicationController
   # GET /messages/new
   # GET /messages/new.xml
   def new
-    @message = Message.new
+    @message = Message.new(:From => "9785719921")
 
     respond_to do |format|
       format.html # new.html.erb
@@ -41,6 +51,15 @@ class MessagesController < ApplicationController
   # POST /messages.xml
   def create
     @message = Message.new(params[:message])
+
+    # Create a Twilio REST account object using your Twilio account ID and token
+    account = Twilio::RestAccount.new(ACCOUNT_SID, ACCOUNT_TOKEN)
+
+    d = { 'From' => @message.From, 'To' => @message.To, 'Body' => @message.Body }
+
+    resp = account.request("/#{API_VERSION}/Accounts/#{ACCOUNT_SID}/SMS/Messages", 'POST', d)
+    resp.error! unless resp.kind_of? Net::HTTPSuccess
+    puts "code: %s\nbody: %s" % [resp.code, resp.body]
 
     respond_to do |format|
       if @message.save
